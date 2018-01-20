bd11707fb0733235b9122c07ef733ee347c4fcf7
Katakeynii <ahmed.abdbassir@gmail.com>
2018-01-20 12:16:11 +0000

    updating the controller

--- src/IslamSn/MosqueBundle/Controller/PrayTabController.php
+++ src/IslamSn/MosqueBundle/Controller/PrayTabController.php
@@ -5,13 +5,26 @@ namespace IslamSn\MosqueBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Route;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Get;
 
 class PrayTabController extends FOSRestController
 {
-	public function  postCreateAction(){
-
+	/**
+	 * POST Route annotation.
+	 * @Post("/praytab/new")
+	 */
+	public function  postPraytabAction(){
+		$repository = $this->getRepository();
 		return "hello";
 	}
+
+	private function getRepository(){
+
+		$em = $this->getDoctrine()->getManager();
+		return $em->getRepository('IslamSnMosqueBundle:PrayTab');
+	}
+
 	public function getEntreprisesAllAction(){
 		return "hello";
 	}
